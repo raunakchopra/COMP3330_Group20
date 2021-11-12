c65d9e8470d2345d627e3e3feea3c7ade541fa38
Raunak Chopra <raunakchopra261@gmail.com>
2021-11-13 05:57:50 +0800

    Added Login Screen

---
+++ frontend/screens/Login.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={require('../assets/logo.jpeg')}
+        style={styles.logo}
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder = "Username"
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder = "Password"
+      />
+      <Text style={styles.footer}>
+          Hike Hong Kong - 2021
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: -3,
+  },
+  logo: {
+      height: 150,
+      width: 150,
+      transform: [{ rotate: '270deg' }],
+      marginBottom: 20,
+  },
+  footer:{
+      top: 250
+  }
+});
